344b0c409960c3efc80f856b122aa6d14114542a
nomaanhusain <32061948+nomaanhusain@users.noreply.github.com>
2020-09-16 14:57:24 +0530

    Update wrlogindone.py

--- wrlogindone.py
+++ wrlogindone.py
@@ -101,8 +101,8 @@ def main_screen():
     global screen
     screen= Tk()
     screen.geometry("300x250")
-    screen.title('Notes 1.0')
-    Label(text="Notes 1.0",bg='grey', width="300", height = "2").pack()
+    screen.title('Restuarant Login')
+    Label(text="Resturant Login",bg='grey', width="300", height = "2").pack()
     Label(text = "").pack()
     Button(text = "Login",height="2", width="30", command = login).pack()
     Label(text = "").pack()
